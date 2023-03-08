fd8d653f92b5a7a18da422caf27cc171d665f3cd
hussainsk786 <hussainsk@yupptv.com>
2023-03-08 23:03:18 +0530

    load method created his 400

--- src/main/java/com/example/demo/Demo2Application.java
+++ src/main/java/com/example/demo/Demo2Application.java
@@ -5,10 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Demo2Application {
+	
+	private Map<String,Integer> map= new HashMap();
+	
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo2Application.class, args);
 		System.out.println("------chnaes 134");
+	}
+	
+	//his 400 chnages
+	publish void loadData(){
+		
 	}
 
 }
